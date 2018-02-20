c40f756995edced4b88655deb7f40ed61cd283b3
Do Hoerin <hellodhlyn@gmail.com>
2018-02-21 02:13:53 +0900

    Added marked for post body, pagination feature on post list page

--- src/components/blog/Index.vue
+++ src/components/blog/Index.vue
@@ -2,9 +2,13 @@
   <div id="blog-index">
     <div class="post container" v-for="post in posts" v-bind:key="post.id">
       <h1 id="post-title"><router-link :to="{ name: 'BlogPost', params: { id: post.id } }">{{ post.title }}</router-link></h1>
-      <p id="post-meta">{{ $d(Date.parse(post.createdAt)) }}</p>
+      <p id="post-meta">{{ $d(Date.parse(post.createdAt)) }} | {{ post.postCategory.name }}</p>
       <p>{{ post.summary }}</p>
     </div>
+    <p id="load-more" v-if="!loading" v-on:click="loadPosts()">
+      <square-button v-if="!noMore" name="+ LOAD MORE"/>
+      <span v-else>마지막 포스트입니다 :)</span>
+    </p>
     <loading-view v-if="loading"/>
   </div>
 </template>
@@ -14,18 +18,36 @@ import axios from 'axios';
 
 export default {
   name: 'BlogIndex',
+  methods: {
+    loadPosts() {
+      this.loading = true;
+
+      let paginationQuery = 'last:20';
+      if (this.posts.length > 0) {
+        paginationQuery += `,before:${this.posts.slice(-1)[0].id}`;
+      }
+
+      axios.get(`${process.env.LYNLAB_BACKEND_HOST}/graphql?query=query{posts(${paginationQuery}){id,title,postCategory{id,name}summary,createdAt}}`)
+        .then((res) => {
+          const results = res.data.data.posts;
+          this.posts.push(...results);
+          if (results.length === 0) {
+            this.noMore = true;
+          }
+
+          this.loading = false;
+        });
+    },
+  },
   data() {
     return {
       posts: [],
-      loading: true,
+      loading: false,
+      noMore: false,
     };
   },
   created() {
-    axios.get(`${process.env.LYNLAB_BACKEND_HOST}/graphql?query=query{posts(last:10){id,title,summary,createdAt}}`)
-      .then((res) => {
-        this.loading = false;
-        this.posts.push(...res.data.data.posts);
-      });
+    this.loadPosts();
   },
 };
 </script>
@@ -38,5 +60,10 @@ export default {
 #post-meta {
   margin-top: 8px;
   color: #9E9E9E;
+}
+
+#load-more {
+  padding: 50px 0;
+  text-align: center;
 }
 </style>
--- src/components/blog/Post.vue
+++ src/components/blog/Post.vue
@@ -2,11 +2,11 @@
   <div id="blog-post">
     <div v-if="post" class="container">
       <h1 id="post-title">{{ post.title }}</h1>
-      <p id="post-meta">{{ $d(Date.parse(post.createdAt)) }}</p>
-      <div id="post-body">{{ post.body }}</div>
+      <p id="post-meta">{{ $d(Date.parse(post.createdAt)) }} | {{ post.postCategory.name }}</p>
+      <div id="post-body" v-html="marked(post.body)"></div>
       <div class="addthis_inline_share_toolbox"></div>
       <div id="post-comments">
-        <vue-disqus shortname="lynlab" :identifier="post.id" />
+        <vue-disqus shortname="lynlab" :identifier="'id=' + post.id" />
       </div>
     </div>
     <error-view v-else-if="loaded" message="포스트를 찾을 수 없음" />
@@ -17,10 +17,12 @@
 <script>
 import VueDisqus from 'vue-disqus/VueDisqus';
 import axios from 'axios';
+import marked from 'marked';
 
 export default {
   name: 'BlogPost',
   components: { VueDisqus },
+  methods: { marked },
   data() {
     return {
       post: null,
@@ -28,7 +30,7 @@ export default {
     };
   },
   created() {
-    axios.get(`${process.env.LYNLAB_BACKEND_HOST}/graphql?query=query{post(id:${this.$route.params.id}){id,title,body,createdAt}}`)
+    axios.get(`${process.env.LYNLAB_BACKEND_HOST}/graphql?query=query{post(id:${this.$route.params.id}){id,title,postCategory{id,name},body,createdAt}}`)
       .then((res) => {
         this.loaded = true;
 
@@ -42,17 +44,42 @@ export default {
 <style scoped>
 #post-title {
   margin-bottom: 0;
+  font-size: 40px;
 }
 
 #post-meta {
   margin-top: 8px;
   color: #9E9E9E;
 }
+</style>
 
+<style>
 #post-body {
-  margin-bottom: 100px;
+  margin: 50px 0 100px 0;
   text-align: left;
-  font-size: 18px;
   line-height: 1.75;
+}
+
+#post-body blockquote {
+  margin: 0;
+  padding: 10px 20px;
+  background-color: #F5F5F5;
+  border-left: #E0E0E0 solid 10px;
+}
+
+#post-body *:not(pre) code {
+  font-size: 16px;
+}
+
+#post-body pre {
+  overflow-y: scroll;
+  padding: 10px 20px;
+  background-color: #F5F5F5;
+}
+
+#post-body img {
+  display: block;
+  margin: 40px auto;
+  max-width: 600px;
 }
 </style>
